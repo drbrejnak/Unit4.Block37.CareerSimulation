e1b9958a8e982ad0eebcf047c7a052a505ec287a
Dani <drbrejnak@gmail.com>
2025-03-23 02:09:32 -0400

    authToken set up for tests

--- server/__tests__/tests.js
+++ server/__tests__/tests.js
@@ -1,8 +1,17 @@
 const request = require('supertest');
 const app = require('../app.js');
 
+let authToken;
+
+beforeAll(async () => {
+    const authResponse = await request(app).post('/api/auth/login').send({
+        username: "moe",
+        password: "m_pw",
+    });
+    authToken = authResponse.body.token;
+});
+
 describe("Api Enpoints", () => {
-    let authToken;
     it('Should register a user', async () => {
         const res = await request(app).post('/api/auth/register').send({
             username: "testuser",
@@ -12,18 +21,17 @@ describe("Api Enpoints", () => {
     });
     it('Should login a user', async () => {
         const res = await request(app).post('/api/auth/login').send({
-            username: "testuser",
-            password: "testpassword",
+            username: "curly",
+            password: "c_pw",
         });
         expect(res.statusCode).toEqual(200);
         expect(res.body).toHaveProperty('token');
-        authToken = res.body.token;
     });
     it("Should return a logged-in user", async () => {
       const res = await request(app).get("/api/auth/me").set(
         'Authorization', `${authToken}`
       );
       expect(res.statusCode).toEqual(200);
-      expect(res.body.username).toBe('testuser');
+      expect(res.body.username).toBe('moe');
     });
 });
